5d3516d09ecbeb065d0d2b4724733e5e5b50f406
Michael Pesce <Michael Pesce>
2017-04-11 14:33:31 -0400

    hangman v1.3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,32 @@
-var anwsers=["gamma bomb", "bruce banner", "betty ross", "thunderbolt ross",
-"abomination", "the leader", "green goliath", "jade giant", "joe fixit",
-"rick jones", "lou ferrigno", "the incredible hulk"];
+var anwserWords=["incredible", "gamma bomb", "bruce banner", "abomination",
+"the leader", "marvel", "anger problems", "green goliath", "rage",
+ "thunderbolt ross"];
+var selectedWord="";
+var lettersInWord=[];
+var numBlanks=0;
+var blanksAndWins=[];
+var wrongLetters=[];
+var lossCount=0;
+var guessesLeft=9;
+var winCount=0;
+var inPlay=false;
 
-var random = Math.floor((Math.random()*(anwsers.length-1)))
+function startGame(){
+	inPlay=true;
+	console.log("Starting Game");
+	selectedWord = anwserWords[Math.floor(Math.random()*anwserWords.length)];
+	lettersInWord = selectedWord.split("");
+	numBlanks = lettersInWord.length;
 
+	guessesLeft=9;
+	wrongLetters=[];
+	blanksAndWins=[];
 
+	for (var i=0; i<numBlanks; i++) {
+		blanksAndWins.push("_");
+	}
+	
+}
 
 document.onkeyup = function(event) {
 	var userGuess = event.ky;
