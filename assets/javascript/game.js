ae2202fc3103c024d4f4c8e4d2b6e2092397b49c
Michael Pesce <Michael Pesce>
2017-04-12 14:41:02 -0400

    hangman v1.4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,28 +7,63 @@ var numBlanks=0;
 var blanksAndWins=[];
 var wrongLetters=[];
 var lossCount=0;
-var guessesLeft=9;
+var guessesLeft=10;
 var winCount=0;
 var inPlay=false;
 
-function startGame(){
+document.onkeyup = function(event) {
+	var userGuess = event.key;
+	console.log(userGuess);
 	inPlay=true;
-	console.log("Starting Game");
-	selectedWord = anwserWords[Math.floor(Math.random()*anwserWords.length)];
-	lettersInWord = selectedWord.split("");
-	numBlanks = lettersInWord.length;
-
-	guessesLeft=9;
+	selectedWord=anwserWords[Math.floor(Math.random()*anwserWords.length)];
+	lettersInWord=selectedWord.split("");
+	numBlanks=lettersInWord.length;
+	guessesLeft=10;
 	wrongLetters=[];
 	blanksAndWins=[];
 
-	for (var i=0; i<numBlanks; i++) {
+	for (var i=0; i<numBlanks; i++){
+		document.getElementById("currentWord").innerHTML=
 		blanksAndWins.push("_");
 	}
+
 	
+	document.getElementById("currentWord").innerHTML=lettersInWord;
+	document.getElementById("guessesLeft").innerHTML=guessesLeft;
+	document.getElementById("winTotal").innerHTML=winCount;
+	document.getElementById("lossTotal").innerHTML=lossCount;
+	document.getElementById("guessesLeft").innerHTML=guessesLeft;
+
+	console.log(selectedWord);
+	console.log(lettersInWord);
+	console.log(numBlanks);
+
+} 
+
+function checksLetter(letter) {
+	var isLetterInWord=false;
+	for(var i=0; i<numBlanks; i++){
+		if(selectedWord[i]===letter) {
+			isLetterInWord = true;
+		}
+	}
+	if(isLetterInWord){
+		for(var i=0;i<numBlanks; i++){
+			if(selectedWord[i]===letter){
+				blanksAndWins[i]=letter;
+			}
+		}
+	}
+	else{
+		wrongLetters.push(letter);
+		guessesLeft--
+	}
 }
 
-document.onkeyup = function(event) {
-	var userGuess = event.ky;
-	console.log(userGuess);
-} 
+
+
+
+
+
+
+
